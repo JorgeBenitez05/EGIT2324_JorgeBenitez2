92505ecf08ef8c539de25d0fb354f95ee2d8a3ea
Jorge Benitez <a_jorge.benitez.llado@iespablopicasso.es>
2024-04-10 09:47:34 +0200

    Añado la clase Persona

---
+++ src/persona/Persona.java
@@ -0,0 +1,25 @@
+package persona;
+
+public class Persona {
+  private String nombre;
+  private String apellidos;
+  private int edad;
+
+  // Constructor
+  public Persona(String nombre, String apellidos, int edad) {
+      this.nombre = nombre;
+      this.apellidos = apellidos;
+      this.edad = edad;
+  }
+
+  public void Cumpleaños() {
+      edad++;
+      System.out.println("Feliz cumpleaños ahora tienes " + edad + " años");
+  }
+
+
+  public static void main(String[] args) {
+      Persona persona = new Persona("Juan", "Pérez", 30);
+      persona.Cumpleaños(); 
+  }
+}
